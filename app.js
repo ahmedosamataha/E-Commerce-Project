c24b497aab66c61bf0e382391057f973c7a6a59c
Aly-Sabie <54023130+Aly-Sabie@users.noreply.github.com>
2020-12-31 16:07:42 +0200

    Update app.js
    Added the automatic data parsing & converted the logos folder to be public

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(bodyParser.urlencoded({ extended: false }));  // To automatically parse data
+app.use(express.static(path.join(__dirname, 'views', 'logos')));  // Converting the logos folder to public, as to access it from the html pages
+
 // Created this function just for testing
 app.use('/', (req, res, next) => {
   res.send('<h1>Hello World! This is our testing html</h1>');
