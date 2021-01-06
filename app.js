bba2e307033e40a959111f0501a2868e2df5c226
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-01-07 00:38:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const sequelize = require('./database/database');
+
+const authRoutes = require('./routes/auth');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
@@ -8,12 +14,25 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));  // To automatically parse data
 app.use(express.static(path.join(__dirname, 'views', 'logos')));  // Converting the logos folder to public, as to access it from the html pages
 
+app.use(authRoutes);
+app.use(shopRoutes);
+
+sequelize
+    .sync()
+    // .sync({force: true})
+    .then(() => {
+        app.listen(3000);
+        console.log('Server started');
+    })
+    .catch(err => console.log(err));
+
 // Created this function just for testing
-app.use('/', (req, res, next) => {
+/*app.use('/', (req, res, next) => {
   res.send('<h1>Hello World! This is our testing html</h1>');
 });
 
-app.listen(3000);
+app.listen(3000);*/
