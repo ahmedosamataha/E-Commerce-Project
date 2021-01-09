1423cc7d20a76c0c8b736fdd4f94cce3e10167a6
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-01-09 16:40:35 +0200

    Update shop.js

--- routes/shop.js
+++ routes/shop.js
@@ -5,6 +5,8 @@ const shopController = require('../controllers/shop');
 
 router.get('/', shopController.getHome);
 
+router.get('/recommendations', shopController.getRecommendations);  //v4
+
 router.get('/products/:productId', shopController.getProduct);
 
 router.post('/add-to-cart', shopController.postAddToCart);
@@ -12,6 +14,15 @@ router.post('/add-to-cart', shopController.postAddToCart);
 router.get('/cart', shopController.getCart);
 
 router.post('/delete-product-from-cart', shopController.deleteProductFromCart);
+
+router.get('/consumer-profile/:consumerId', shopController.getProfile); // v2
+
+router.post('/update-profile', shopController.postUpdateProfile); // v2
+
+router.get('/place-order', shopController.getPlaceOrder); // v2
+
+router.post('/place-order', shopController.postPlaceOrder); // v2
+router.post('/place-order', shopController.postCreateOrder); // v3
 
 // router.get('/sign-in', shopController.getHome);
 
