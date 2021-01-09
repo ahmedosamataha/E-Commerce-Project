808b2726b29b241a74b96de8390fc93cf3dd648a
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-01-09 13:46:30 +0200

    Update consumer.js

--- models/consumer.js
+++ models/consumer.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 
+const Order = require('./order'); //v2
 const Cart = require('./cart');
 const ConsumerTable = require('../database/tables/consumer');
 
@@ -17,11 +18,15 @@ class Consumer {
     getPassword() { return this.#consumer.password; }
     getEmail() { return this.#consumer.email; }
     getGender() { return this.#consumer.gender; }
+    getShippingAddress() { return this.#consumer.shippingAddress; } //v3
+    getLastVisited() {return this.#consumer.lastVisited; } //v4
 
     setName(name) { this.#consumer.name = name; }
     setPassword(password) { this.#consumer.password = password; }
     setEmail(email) { this.#consumer.email = email; }
     setGender(gender) { this.#consumer.gender = gender; }
+    setShippingAddress(shippingAddress) { this.#consumer.shippingAddress = shippingAddress; }   //v3
+    setLastVisited(lastVisited) {this.#consumer.lastVisited = lastVisited; } //v4
 
     createCart() {
         return this.#consumer
@@ -30,10 +35,22 @@ class Consumer {
                         return Cart.wrapUp(cart);
                     });
     }
-
+                                 
+    createOrder(prop) {  //v2
+        return this.#consumer
+                    .createOrder(prop)
+                    .then(order => {
+                        return Order.wrapUp(order);
+                    });
+    }
+                                 
     destroy() {
         return this.#consumer.destroy();
     }
+
+    save() {    // v2
+            return this.#consumer.save();
+        }
 
     getCart() {
         return this.#consumer
@@ -41,6 +58,15 @@ class Consumer {
                     .then(cart => {
                         return Cart.wrapUp(cart);
                     });
+    }
+    
+    getOrders(searchProp) { //v2
+        return this.#consumer
+                    .getOrders(searchProp)
+                    .then(orders => {
+                        return Order.wrapUp(orders);
+                    })
+                    // .catch(console.log('consumer.getOrders', err));
     }
 
     static getDB() {
