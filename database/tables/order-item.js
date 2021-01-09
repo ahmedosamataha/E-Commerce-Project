8d00972597fbbf96d5fc64d8816f08c5afb815b1
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-01-09 13:50:23 +0200

    Update order-item.js

--- database/tables/order-item.js
+++ database/tables/order-item.js
@@ -1,59 +1,22 @@
 const Sequelize = require('sequelize');
 
-const OrderItemTable = require('../database/tables/order-item');
+const sequelize = require('../init');
 
-class OrderItem {
-    #orderItem;
-
-    constructor (orderItem) {
-        this.#orderItem = orderItem;
+const OrderItemTable = sequelize.define('orderItem', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: true
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    discount: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
     }
+});
 
-    getOrderItemData() {return this.#orderItem;}
-
-    getId() {return this.#orderItem.id;}
-    getQuantity() {return this.#orderItem.quantity;}
-    getDiscount() {return this.#orderItem.discount;}
-
-    setQuantity(quantity) {this.#orderItem.quantity = quantity;}
-    setDiscount(discount) {this.#orderItem.discount = discount;}
-
-    destroy() {
-        return this.#orderItem.destroy();
-    }
-
-    save() {
-        return this.#orderItem.save();
-    }
-
-    static wrapUp(orderItem) {
-        if (Array.isArray(orderItem))
-            return orderItem.map(c => {
-                if (c)
-                    return new Order(c);
-                return c;
-            });
-        if (orderItem)
-            return new OrderItem(orderItem);
-        return orderItem;
-    }
-
-    static findById(id) {return OrderItem.findByPk(id);}
-    static findByPk(id) {
-        return OrderItemTable
-                .findByPk(id)
-                .then(orderItem => {
-                    return this.wrapUp(orderItem);
-                });
-    }
-
-    static findAll(searchProp) {
-        return OrderItemTable.findAll(searchProp);
-    }
-
-    static getDB() {
-        return OrderItemTable;
-    }
-}
-
-module.exports = OrderItem;
+module.exports = OrderItemTable;
