adb1a66fece8b7c46868c92ac84a196921c37aae
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-01-12 12:56:52 +0200

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,12 +1,12 @@
 const sequelize = require('./init');
 
+const seller = require('./tables/seller');  //v7
 const consumer = require('./tables/consumer');
 const product = require('./tables/product');
 const cart = require('./tables/cart');
 const cartItem = require('./tables/cart-item');
 const order = require('./tables/order'); //v2 
 const orderItem = require('./tables/order-item');//v2
-const seller = require('./tables/seller');
 
 consumer.hasOne(cart);
 cart.belongsTo(consumer, {constraint: true, onDelete: 'CASCADE'}); // v3
@@ -19,5 +19,8 @@ cart.belongsToMany(product, {through: cartItem});
 
 product.belongsToMany(order, {through: orderItem}); // v2
 order.belongsToMany(product, {through: orderItem}); // v2
+
+seller.hasMany(product);    //v7
+product.belongsTo(seller);  //v7
 
 module.exports = sequelize;
