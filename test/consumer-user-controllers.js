320185733e8d145052ad7a6968c7709833be6202
SamuelShokry <68712819+SamuelShokry@users.noreply.github.com>
2021-01-12 12:46:49 +0200

    Update and rename shop-controller.js to consumer-user-controllers.js

--- test/consumer-user-controllers.js
+++ test/consumer-user-controllers.js
@@ -21,20 +21,26 @@ function areEqual(consumer, testConsumer) {
 
 function areEqualProducts(product, testProduct) {
     const name = product.getName();
-    const quantity = +product.getQuantity();
     const tag = product.getTag();
     const description = product.getDescription();
     const imageUrl = product.getImageUrl();
     const price = +product.getPrice();
     expect(name).to.be.equal(testProduct.name);
-    expect(quantity).to.be.equal(testProduct.quantity);
     expect(tag).to.be.equal(testProduct.tag);
     expect(description).to.be.equal(testProduct.description);
     expect(imageUrl).to.be.equal(testProduct.imageUrl);
     expect(price).to.be.equal(testProduct.price);
 }
 
-describe('Shop - controller', function () {
+function printProduct(product) {
+    console.log(product.getName());
+    console.log(product.getQuantity());
+    console.log(product.getTag());
+    console.log(product.getImageUrl());
+    console.log(product.getDescription());
+}
+
+describe('Consumer user - controllers', function () {
     const testConsumer = {
         email: 'test@test.comshop',
         password: 'testshop',
@@ -53,29 +59,64 @@ describe('Shop - controller', function () {
     
     let req;
     
-    before(function (done) {
-        Consumer
-            .create(testConsumer)
-            .then(consumer => {
-                testConsumer.id = consumer.getId();
-                req = {
-                    cookies: {
-                        userId: testConsumer.id,
-                        userType: 'consumer'
-                    }
-                }
-                done();
-            })
+    // before(function (done) {
+    //     Consumer
+    //         .create(testConsumer)
+    //         .then(consumer => {
+    //             testConsumer.id = consumer.getId();
+    //             req = {
+    //                 cookies: {
+    //                     userId: testConsumer.id,
+    //                     userType: 'consumer'
+    //                 }
+    //             }
+    //             done();
+    //         })
+    // })
+
+    it('should sign up testConsumer', function(done) {
+        const req = {
+            body: {
+                type: 'consumer', 
+                ...testConsumer, 
+                repassword: testConsumer.password
+            }
+        };
+        const res = {
+            redirect: (str) => {
+                expect(str).to.equal('/sign-in');
+                Consumer
+                    .findAll({where: {email: testConsumer.email}})
+                    .then(consumers => {
+                        // console.log(consumers[0])
+                        areEqual(consumers[0], testConsumer);
+                        testConsumer.id = consumers[0].getId();
+                        done();
+                    })
+            }
+        }
+        authControllers.postSignUpConsumer(req, res, () => {})
     })
 
-    it('should create a cart for the tester', function(done) {
-        Consumer
-            .findById(testConsumer.id)
-            .then(consumer => consumer.createCart())
-            .then(cart => {
-                expect(cart).not.to.equal(null);
+    it('should sign testConsumer in', function(done) {
+        req = {
+            body: {
+                email: testConsumer.email,
+                password: testConsumer.password,
+            }
+        };
+        const cookies = {};
+        const res = {
+            cookie: function(name, value) {
+                cookies[name] = value;
+            },
+            redirect: () => {
+                expect(cookies).to.have.property('userType', 'consumer');
+                expect(cookies).to.have.property('userId', testConsumer.id);
                 done();
-            })
+            }
+        }
+        authControllers.postSignInConsumer(req, res, () => {})
     })
 
     it('should create a test product for the test', function(done) {
@@ -89,8 +130,14 @@ describe('Shop - controller', function () {
         })
     });
 
-    it('should find my account', function(done) {
+    it('should find my account data an put it in the req object', function(done) {
         // console.log('req before', req)
+        req = {
+            cookies: {
+                userId: testConsumer.id,
+                userType: 'consumer'
+            }
+        }
         authControllers.findUser(req, {}, () => {
             areEqual(req.user, testConsumer);
             done();
@@ -128,6 +175,49 @@ describe('Shop - controller', function () {
         shopControllers.postUpdateProfile(req, res, () => {});
     })
 
+    it('should add a product to my cart', function(done) {
+        req.body = {
+            productId: testProduct.id,
+            quantity: '10'
+        }
+        const res = {
+            redirect: function(path) {
+                expect(path).to.equal('/');
+                done();
+            }
+        }
+
+        shopControllers.postAddToCart(req, res, () => {});
+    }) 
+
+    it('should return the products in my cart', function(done) {
+        const res = {
+            render: function(path, data) {
+                areEqualProducts(data.products[0], testProduct);
+                done();
+            }
+        }
+
+        shopControllers.getCart(req, res, () => {});
+    })
+
+    it('should create an order with the products in my cart', function(done) {
+        req.body = {
+            email: testConsumer.email
+        }
+        const res = {
+            render: function(path, data) {
+                areEqualProducts(data.products[0], testProduct);
+                done();
+            }
+        }
+        const next = () => {
+            shopControllers.postCreateOrder(req, res, () => {})
+        }
+
+        shopControllers.postPlaceOrder(req, res, next);
+    })
+
     it('should find my recommendations', function(done) {
         const res = {
             render: function(path, data) {
@@ -150,6 +240,21 @@ describe('Shop - controller', function () {
             })
     })
      
+    it('should delete the cookies', function() {
+        let cookiesToBeDeleted = [];
+        let res = {
+            clearCookie: (str) => {
+                cookiesToBeDeleted.push(str);
+            },
+            redirect: function() {}
+        }
+        authControllers.signOut({}, res, () => {})
+        // console.log(cookiesToBeDeleted);
+        expect(cookiesToBeDeleted).to.have.property('length', 2);
+        expect(cookiesToBeDeleted[0]).to.equal('userId');
+        expect(cookiesToBeDeleted[1]).to.equal('userType');
+        // done();
+    })
 
     it('should delete the test product', function(done) {
         Product
